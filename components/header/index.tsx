f9ba0cdb87fb12f2e4accd9ef28c5e9596a6ee1f
batuhanbostanci <batuhanbostanci2@hotmail.com>
2025-04-13 15:31:56 +0300

    Adding dark mode to the project

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constanst";
+import ModeToogle from "./mode-toggle";
 
 const Header = () => {
   return (
@@ -23,12 +24,13 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToogle></ModeToogle>
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart /> Cart
             </Link>
           </Button>
-          <Button asChild variant="ghost">
+          <Button asChild>
             <Link href="/sign-in">
               <UserIcon /> Sign In
             </Link>
